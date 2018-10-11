ebed10d926f4f516d5308177897f02e4e72a4425
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2018-10-11 12:19:46 -0400

    changed sidebar info, package.json has newest package versions

--- client/components/SideBar.vue
+++ client/components/SideBar.vue
@@ -7,9 +7,9 @@
 
     <h5>
       
-      <p>The Section 208 Areawide Plan Update (208 Plan) provides nitrogen-removal target goals for the Cape’s impaired waters. These targets are based on extensive monitoring and analysis that was completed as part of the Massachusetts Estuaries Project (MEP).</p>
-      <p>Since the publication of these estuary-specific MEP Reports, most Cape towns have continued to collect water quality data and conduct additional studies for many of their estuaries. This has resulted in a substantial number of embayment-specific water quality data sets that span over fifteen years.</p>
-      <p>Going forward, the 208 Plan recommends extensive monitoring of watershed solutions to ensure water quality goals are being met. The Water Quality Monitoring application aims to provide an analysis of Cape Cod estuaries' vital nutrients.</p>
+      <p>The Cape Cod Area-Wide Water Quality Management Plan (208 Plan) provides a framework for achieving nitrogen removal targets in Cape Cod’s impaired coastal waters.  These targets are based on analyses completed by the Massachusetts Estuaries Project (MEP).</p>
+      <p>Since publication of estuary-specific MEP reports, many communities, organizations and agencies have continued to estuarine water quality data and conduct additional studies.  The result is historic embayment-specific water quality data sets dating back 15 years.  The 208 Plan recommends a regional monitoring program that supports collection and analysis of data to inform local decisions and investments in water quality infrastructure. </p>
+      <p>This regional water quality monitoring application includes data collected and analyzed by various organizations, agencies and institutions, including the Buzzards Bay Coalition, Center for Coastal Studies, the School for Marine Science and Technology at UMass-Dartmouth, Cape Cod towns, and others.  Data from these entities has been compiled by the Cape Cod Commission into this regional database.</p>
       <p>
         <button @click = 'downloadExcel'  class = 'btn btn-success'>Download Blank Template</button>
       </p>
